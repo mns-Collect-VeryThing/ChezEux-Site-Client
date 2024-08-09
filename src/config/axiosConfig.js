2f8995c17aefff642d0701d2b04a0f2c7c606f56
Bastien <bastien.biblocque@gmail.com>
2024-08-09 10:16:47 +0200

    :sparkles: Utilisation API microservice

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://66b07fc66a693a95b538edbe.mockapi.io', // Remplacez par votre base URL
+    baseURL: 'https://localhost:7004/api/app', // Remplacez par votre base URL
+    // baseURL: 'https://66b07fc66a693a95b538edbe.mockapi.io', // Remplacez par votre base URL
     timeout: 1000,
     headers: {'Content-Type': 'application/json'},
 });
