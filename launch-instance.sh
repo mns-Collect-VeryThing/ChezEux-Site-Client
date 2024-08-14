7ad905c993befc4e29d947db2942e9d370e6817e
Bastien <bastien.biblocque@gmail.com>
2024-08-14 12:49:22 +0200

    :sparkles: .env config

--- launch-instance.sh
+++ launch-instance.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
-cd /Users/bastien/PhpstormProjects/ChezEux-Site-Client
-git pull
 
 # Vérifier si les arguments nécessaires sont fournis
-if [ "$#" -ne 3 ]; then
-  echo "Usage: $0 THEME PORT LANG"
+if [ "$#" -ne 5 ]; then
+  echo "Usage: $0 ID NAME THEME PORT LANG"
   exit 1
 fi
 
 # Lire les arguments
-THEME=$1
-PORT=$2
-LANG=$3
+ID=$1
+NAME=$2
+THEME=$3
+PORT=$4
+LANG=$5
+
+# Fichier .env
+ENV_FILE=".env"
+
+# Supprimer le fichier .env s'il existe
+if [ -f "$ENV_FILE" ]; then
+  rm "$ENV_FILE"
+fi
+
+# Créer un nouveau fichier .env et ajouter les variables
+{
+  echo "REACT_APP_ID=$ID"
+  echo "REACT_APP_NAME=$NAME"
+  echo "REACT_APP_THEME=$THEME"
+  echo "REACT_APP_PORT=$PORT"
+  echo "REACT_APP_LANG=$LANG"
+} > "$ENV_FILE"
 
 # Créer un fichier docker-compose temporaire
 cat > docker-compose-temp.yml <<EOL
 version: '3.8'
 
 services:
-  react-app-$THEME-$LANG-$PORT:
+  react-app-$ID-$NAME-$THEME-$LANG-$PORT:
     build:
       context: .
       args:
@@ -28,6 +45,8 @@ services:
       - "$PORT:3000"
     environment:
       NODE_ENV: production
+      ID: $ID
+      NAME: $NAME
 EOL
 
 # Lancer le service avec Docker Compose
@@ -36,4 +55,4 @@ docker-compose -f docker-compose-temp.yml up -d
 # Supprimer le fichier temporaire
 rm docker-compose-temp.yml
 
-echo "Instance lancée avec succès avec le thème $THEME et la langue $LANG sur le port $PORT"
+echo "Instance lancée avec succès avec l'ID $ID, le nom $NAME, le thème $THEME, la langue $LANG sur le port $PORT"
